a635da41cb39e683ecbc576330f128f3dbbe5fde
Bob Pulaski <mynameisempty@yandex.ru>
2021-11-15 15:01:58 +0300

    15-11-21

--- blog/app/Http/Controllers/AddOrganizationController.php
+++ blog/app/Http/Controllers/AddOrganizationController.php
@@ -6,9 +6,16 @@ use Illuminate\Http\Request;
 
 class AddOrganizationController extends Controller
 {
-    function add(){
-        //return "kuku";
-        dd(url());
+    function add1(Request $request){
+
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'inn' => 'required',
+        ]);
+
+        $name = $request->input('name');
+        $inn = $request->input('inn');
+        dd($name);
     }
 
 }
--- blog/resources/views/add_organization_form.blade.php
+++ blog/resources/views/add_organization_form.blade.php
@@ -40,30 +40,43 @@
             <form action="{{ route('organization.add') }}" method="post">
                 @csrf
                 <div class="flex flex-col bg-white p-10 rounded-lg shadow space-y-6">
-                    <h1 class="font-bold text-xl text-center">Sign in to your account</h1>
+                    <h1 class="font-bold text-xl text-center">Добавить организацию</h1>
 
                     <div class="flex flex-col space-y-1">
-                        <input type="text" name="username" id="username" class="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow" placeholder="E-mail" />
+                        <label for="e-mail">Наименование</label>
+                        <input type="text" name="name" id="name" class="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow" placeholder="Наименование" />
                     </div>
 
                     <div class="flex flex-col space-y-1">
-                        <input type="password" name="password" id="password" class="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow" placeholder="Password" />
+                        <label for="e-mail">ИНН</label>
+                        <input type="text" name="inn" id="inn" class="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow" placeholder="ИНН" />
                     </div>
 
-                    <div class="relative">
+ {{--                   <div class="relative">
                         <input type="checkbox" name="remember" id="remember" checked class="inline-block align-middle" />
                         <label class="inline-block align-middle" for="remember">Remember me</label>
-                    </div>
+                    </div>--}}
 
                     <div class="flex flex-col-reverse sm:flex-row sm:justify-between items-center">
-                        <a href="#" class="inline-block text-blue-500 hover:text-blue-800 hover:underline">Forgot your password?</a>
-                        <button type="submit" class="bg-blue-500 text-white font-bold px-5 py-2 rounded focus:outline-none shadow hover:bg-blue-700 transition-colors">Log In</button>
+                        {{--<a href="#" class="inline-block text-blue-500 hover:text-blue-800 hover:underline">Forgot your password?</a>--}}
+                        <button type="submit" class="bg-blue-500 text-white font-bold px-5 py-2 rounded focus:outline-none shadow hover:bg-blue-700 transition-colors">Добавить</button>
                     </div>
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                 </div>
             </form>
-            <div class="flex justify-center text-gray-500 text-sm">
+            {{--<div class="flex justify-center text-gray-500 text-sm">
                 <p>&copy;2021. All right reserved.</p>
-            </div>
+            </div>--}}
         </div>
     </div>
 
