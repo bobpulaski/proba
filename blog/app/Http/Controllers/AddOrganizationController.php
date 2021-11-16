825fddef92980eff0057072160b3e23ba5e833fc
Bob Pulaski <mynameisempty@yandex.ru>
2021-11-16 13:55:24 +0300

    '16-11-21'

--- blog/app/Http/Controllers/AddOrganizationController.php
+++ blog/app/Http/Controllers/AddOrganizationController.php
@@ -3,19 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Organizations;
 
 class AddOrganizationController extends Controller
 {
-    function add(Request $request){
+    function add(Request $request)
+    {
 
         $this->validate($request, [
             'name' => 'required|max:255',
             'inn' => 'required',
         ]);
 
-        $name = $request->input('name');
-        $inn = $request->input('inn');
-        dd($name, $inn);
+        $Organization = new Organizations;
+        $Organization->name = $request->input('name');
+        $Organization->inn = $request->input('inn');
+        $Organization->save();
+
+        //dd($name, $inn);
+        //DB::insert('insert into organizations (name, inn) values (?, ?)', [$name, $inn]);
     }
 
 }
